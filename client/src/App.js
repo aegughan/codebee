3e2cb58e32325a98de21aaf25982077e443e2452
Gughan <aegughan@gmail.com>
2021-12-16 01:19:14 +0530

    App menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,30 @@ import './App.css';
 import React from 'react';
 import Editor from './components/Editor/Editor';
 import MenuBar from './components/MenuBar/MenuBar';
+import AppMenu from './components/AppMenu/AppMenu';
 import { AppContext } from './context/AppContext';
+import Sudoku from './components/Sudoku/Sudoku';
 
 function App() {
   const [editorProperties, setEditorProperties] = React.useState({
     selectedLanguage: 'html',
     fontSize: '12',
     onExecuteCode: () => {},
+    selectedAppMenu: 'Code Play'
   })
   return (
     <div> 
       <AppContext.Provider value={editorProperties}>
-        <MenuBar setEditorProperties={setEditorProperties}/>
-        <Editor setEditorProperties={setEditorProperties}/>
+        <AppMenu setEditorProperties={setEditorProperties}/> 
+        {editorProperties.selectedAppMenu === 'Code Play' && 
+        <>
+          <MenuBar setEditorProperties={setEditorProperties}/>
+          <Editor setEditorProperties={setEditorProperties}/>
+        </>
+        }
+        {editorProperties.selectedAppMenu === 'Sudoku' &&
+        <Sudoku />
+        }
       </AppContext.Provider>
      </div>
   );
